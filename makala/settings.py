b647d42114e2475fdf7ea2f0b7455abb0eb26f3a
Arslan <91959072+rasulow@users.noreply.github.com>
2024-08-26 23:15:15 +0500

    debug true

--- makala/settings.py
+++ makala/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*^mjp_qfgnyoqb%guse8#*9i3!5$jr76or%@tn)5nhsvio7k*('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
